ba73347bf929d1c485c3d20f06f61b71bca46bde
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-06-05 14:33:01 +0100

    Added required npm dependencies
    Using both get-pixels and get-rgba-palette to grab image data and create a colour palette out of it. This commit also contains temporary testing code that I used to get used to the dependencies syntax.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,73 @@ import React, { Component } from 'react';
 //Import SASS Files
 import './App.scss';
 
+const generatePalette = require('get-rgba-palette');
+const getPixels = require('get-pixels');
+
 class App extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = { hasGeneratedPalette: false };
+    this.colourPalette = [];
+
+    this.testFunc();
+  }
+
+
+  testFunc = () =>
+  {
+    getPixels('https://pbs.twimg.com/media/DRfqQX4VQAErefq.jpg:large', (err, pixels) =>
+    {
+      if (err)
+      {
+        console.log("Bad Image Path");
+
+        this.setState({ hasGeneratedPalette: false });
+      }
+      else
+      {
+        this.colourPalette = generatePalette(pixels.data, 5, 1000);
+
+        console.table(this.colourPalette);
+
+        this.setState({ hasGeneratedPalette: true });
+      }
+    });
+  }
+
+  renderColourPalette = () =>
+  {
+    if (this.state.hasGeneratedPalette)
+    {
+      return (
+        <div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}></div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}></div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}></div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}></div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}></div>
+
+        </div>
+      );
+    }
+    else
+    {
+      return (
+        <div> No colour palette has been generated</div>
+      );
+    }
+
+  }
+
+
   render()
   {
     return (
       <div className="App">
-        Hello World!
+        {this.renderColourPalette()}
       </div>
     );
   }
