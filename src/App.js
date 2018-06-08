ace178eb10084ac026cfa507e167cc23ced072ca
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-06-08 17:25:54 +0100

    Implemented gradient component
    Takes two props the first and second colours, and will create a linear css gradient that fills its bounding box using those two colours from top to bottom respectively

--- src/App.js
+++ src/App.js
@@ -1,78 +1,80 @@
 // ! Entry point for all of the other components (either through another component or directly).
 
 //Import React
-import React, { Component } from 'react';
+import React from 'react';
 
 //Import SASS Files
 import './App.scss';
 
+//Import Components
+import Gradient from './components/Gradient.js';
+
 const generatePalette = require('get-rgba-palette');
 const getPixels = require('get-pixels');
 
-class App extends Component
+class App extends React.Component
 {
   constructor(props)
   {
     super(props);
 
-    this.state = { hasGeneratedPalette: false };
+    this.state = { hasGeneratedPalette: false, firstColour: "red", secondColour: "green" };
     this.colourPalette = [];
-
-    this.testFunc();
   }
 
 
-  testFunc = () =>
-  {
-    getPixels('https://pbs.twimg.com/media/Dep_oEnX0AAWcfH.jpg:large', (err, pixels) =>
-    {
-      if (err)
-      {
-        console.log("Bad Image Path");
+  // testFunc = () =>
+  // {
+  //   getPixels('https://pbs.twimg.com/media/Dep_oEnX0AAWcfH.jpg:large', (err, pixels) =>
+  //   {
+  //     if (err)
+  //     {
+  //       console.log("Bad Image Path");
 
-        this.setState({ hasGeneratedPalette: false });
-      }
-      else
-      {
-        this.colourPalette = generatePalette(pixels.data, 5, 1000);
+  //       this.setState({ hasGeneratedPalette: false });
+  //     }
+  //     else
+  //     {
+  //       this.colourPalette = generatePalette(pixels.data, 5, 1000);
 
-        console.table(this.colourPalette);
+  //       console.table(this.colourPalette);
 
-        this.setState({ hasGeneratedPalette: true });
-      }
-    });
-  }
+  //       this.setState({ hasGeneratedPalette: true });
+  //     }
+  //   });
+  // }
 
-  renderColourPalette = () =>
-  {
-    if (this.state.hasGeneratedPalette)
-    {
-      return (
-        <div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}></div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}></div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}></div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}></div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}></div>
+  // renderColourPalette = () =>
+  // {
+  //   if (this.state.hasGeneratedPalette)
+  //   {
+  //     return (
+  //       <div>
+  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}></div>
+  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}></div>
+  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}></div>
+  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}></div>
+  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}></div>
 
-        </div>
-      );
-    }
-    else
-    {
-      return (
-        <div> No colour palette has been generated</div>
-      );
-    }
+  //       </div>
+  //     );
+  //   }
+  //   else
+  //   {
+  //     return (
+  //       <div> No colour palette has been generated</div>
+  //     );
+  //   }
 
-  }
+  // }
 
 
   render()
   {
     return (
       <div className="App">
-        {this.renderColourPalette()}
+        <Gradient
+        <Gradient firstColour={this.state.firstColour} secondColour={this.state.secondColour} />
       </div>
     );
   }
