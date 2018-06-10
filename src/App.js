ad302e4d5f8676fb096fa5590a558de7639ccaf8
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-06-10 16:47:39 +0100

    Added Functionality to get two colours from an image

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Card from './components/Card.js';
 import Gradient from './components/Gradient.js';
 import Button from './components/Button.js';
 
-const generatePalette = require('get-rgba-palette');
-const getPixels = require('get-pixels');
+import generatePalette from 'get-rgba-palette';
+import getPixels from 'get-pixels';
 
 class App extends React.Component
 {
@@ -20,69 +20,63 @@ class App extends React.Component
   {
     super(props);
 
-    this.state = { hasGeneratedPalette: false, firstColour: "#234359", secondColour: "#7C7C74" };
+    this.state =
+      {
+        hasGeneratedPalette: false, firstColour: "#075372", secondColour: "#848784"
+      };
+
     this.colourPalette = [];
+
+    this.getColours("https://pbs.twimg.com/media/DfNpaMWW0AES_qg.jpg:large");
   }
 
-  //#region colour palette code
-  // testFunc = () =>
-  // {
-  //   getPixels('https://pbs.twimg.com/media/Dep_oEnX0AAWcfH.jpg:large', (err, pixels) =>
-  //   {
-  //     if (err)
-  //     {
-  //       console.log("Bad Image Path");
+  getColours = (url) =>
+  {
+    getPixels(url, (err, pixels) =>
+    {
+      if (err)
+      {
+        console.error("Bad Image Path");
+      }
+      else
+      {
+        this.colourPalette = generatePalette(pixels.data, 5, 20);
 
-  //       this.setState({ hasGeneratedPalette: false });
-  //     }
-  //     else
-  //     {
-  //       this.colourPalette = generatePalette(pixels.data, 5, 1000);
+        this.setState({ hasGeneratedPalette: true, firstColour: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")", secondColour: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" });
+      }
+    });
+  }
 
-  //       console.table(this.colourPalette);
 
-  //       this.setState({ hasGeneratedPalette: true });
-  //     }
-  //   });
-  // }
-
-  // renderColourPalette = () =>
-  // {
-  //   if (this.state.hasGeneratedPalette)
-  //   {
-  //     return (
-  //       <div>
-  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}></div>
-  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}></div>
-  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}></div>
-  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}></div>
-  //         <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}></div>
-
-  //       </div>
-  //     );
-  //   }
-  //   else
-  //   {
-  //     return (
-  //       <div> No colour palette has been generated</div>
-  //     );
-  //   }
-
-  // }
-  //#endregion
-
+  renderColourPalette = () =>
+  {
+    if (this.state.hasGeneratedPalette)
+    {
+      return (
+        <div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}>.</div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}>.</div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}>.</div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}>.</div>
+          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}>.</div>
+        </div>
+      );
+    }
+  }
 
   render()
   {
     return (
-      <div id="App" style={{ backgroundColor: this.state.firstColour }}>
+      <div id="App" style={{ backgroundColor: "#2C3A47" }}>
+        <h1 id="title"> GeoColours </h1>
+
         <Card>
           <Gradient firstColour={this.state.firstColour} secondColour={this.state.secondColour} />
           <div id="Buttons">
             <div className="button">
               <Button width="100px" height="40px" backgroundColour="#fff" onClick={() => console.log("Button Clicked!")}>
-                <img src={require("./icons/code.svg")} alt="View CSS Code Icon" />
-                View CSS
+                <img src={require("./icons/code.svg")} alt="Copy CSS Code Icon" />
+                Copy CSS
               </Button>
             </div>
             <div className="button">
