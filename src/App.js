bddd0f74420fa8e3e870dd59572130a78138d788
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-06-21 11:35:23 +0100

    Stored reddit images and misc changes
    Reddit images are now stored when initial hot.json is grabbed, speeds it up a ton

--- src/App.js
+++ src/App.js
@@ -27,18 +27,16 @@ class App extends React.Component
         hasGeneratedPalette: false, firstColour: "#075372", secondColour: "#848784"
       };
 
+    this.images = [];
+    this.currentImage = 0;
     this.colourPalette = [];
 
     this.sendRedditRequest();
-
-    this.index = 0;
   }
 
 
   sendRedditRequest = () =>
   {
-    // ! reddit.setupOAuth2("D_htB7b-H13HRg", "aObzp_MljVBhAwxXcfD1Uqhhedw", "https://pjaerr.github.io/GeoColours");
-
     request("https://www.reddit.com/r/EarthPorn/hot.json?&jsonp", (error, response, body) =>
     {
       if (error)
@@ -49,78 +47,85 @@ class App extends React.Component
 
       body = JSON.parse(body);
 
-      let source = body.data.children[this.index].data.preview.images[0].source;
-      console.log(source);
+      this.grabImagesFromReddit(body.data.children);
 
-
-      this.getColours(window.location.href + "?url=" + source.url);
-
-      this.index++;
+      this.generateGradient();
     });
   }
 
-  getColours = (url) =>
+  grabImagesFromReddit = (posts) =>
   {
-    if (this.ctx === null || this.ctx === undefined)
+    for (let i = 0; i < posts.length; i++)
     {
-      this.ctx = this.canvas.getContext("2d");
+      this.images.push(window.location.href + '?url=' + posts[i].data.preview.images[0].source.url);
     }
-
-    let image = new Image(100, 100);
-    image.src = url;
-
-    image.onload = () =>
-    {
-      console.log("Loaded image");
-
-      this.canvas.width = 100;
-      this.canvas.height = 100;
-
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-      this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
-
-
-      let data = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-
-      this.colourPalette = generatePalette(data.data, 5, 20);
-
-      this.setState({ hasGeneratedPalette: true, firstColour: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")", secondColour: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" });
-    }
-
-
-    // getPixels(url, (err, pixels) =>
-    // {
-    //   if (err)
-    //   {
-    //     console.error(err);
-    //   }
-    //   else
-    //   {
-    //     console.log(pixels);
-    //     this.colourPalette = generatePalette(pixels.data, 5, 20);
-
-    //     this.setState({ hasGeneratedPalette: true, firstColour: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")", secondColour: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" });
-    //   }
-    // });
   }
 
+  generateGradient = () =>
+  {
+    this.updateGradient(this.images[this.currentImage]);
+
+    if (this.currentImage < this.images.length)
+    {
+      this.currentImage++;
+    }
+    else
+    {
+      this.currentImage = 0;
+    }
+  }
+
+  updateGradient = (url) =>
+  {
+    getPixels(url, (err, pixels) =>
+    {
+      if (err)
+      {
+        console.error(err);
+      }
+      else
+      {
+        this.colourPalette = generatePalette(pixels.data, 5, 20);
+
+        this.setState({ hasGeneratedPalette: true, firstColour: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")", secondColour: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" });
+      }
+    });
+  }
+
+  copyGradientCode = () =>
+  {
+    let firstColour = this.state.firstColour;
+    let secondColour = this.state.secondColour;
+
+    let code = "background:" + firstColour + ";\n background: -webkit-linear-gradient(to bottom, " + firstColour + ", " + secondColour + "); \n background: linear-gradient(to bottom, " + firstColour + ", " + secondColour + "); ";
+
+    console.log("Copied Code!");
+
+    const textField = document.createElement('textarea');
+    textField.innerText = code;
+    const parentElement = document.getElementById("root");
+    parentElement.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    parentElement.removeChild(textField);
+  }
 
   renderColourPalette = () =>
   {
     if (this.state.hasGeneratedPalette)
     {
       return (
-        <div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}>.</div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}>.</div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}>.</div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}>.</div>
-          <div style={{ height: "100px", backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}>.</div>
+        <div className="palette-container">
+          <div className="palette-container-colour" style={{ backgroundColor: "rgb(" + this.colourPalette[0][0] + "," + this.colourPalette[0][1] + "," + this.colourPalette[0][2] + ")" }}></div>
+          <div className="palette-container-colour" style={{ backgroundColor: "rgb(" + this.colourPalette[1][0] + "," + this.colourPalette[1][1] + "," + this.colourPalette[1][2] + ")" }}></div>
+          <div className="palette-container-colour" style={{ backgroundColor: "rgb(" + this.colourPalette[2][0] + "," + this.colourPalette[2][1] + "," + this.colourPalette[2][2] + ")" }}></div>
+          <div className="palette-container-colour" style={{ backgroundColor: "rgb(" + this.colourPalette[3][0] + "," + this.colourPalette[3][1] + "," + this.colourPalette[3][2] + ")" }}></div>
+          <div className="palette-container-colour" style={{ backgroundColor: "rgb(" + this.colourPalette[4][0] + "," + this.colourPalette[4][1] + "," + this.colourPalette[4][2] + ")" }}></div>
         </div>
       );
     }
   }
+
 
   render()
   {
@@ -132,21 +137,21 @@ class App extends React.Component
           <Gradient firstColour={this.state.firstColour} secondColour={this.state.secondColour} />
           <div id="Buttons">
             <div className="button">
-              <Button width="100px" height="40px" backgroundColour="#fff" onClick={this.sendRedditRequest}>
+              <Button width="100px" height="40px" backgroundColour="#fff" onClick={this.copyGradientCode}>
                 <img src={require("./icons/code.svg")} alt="Copy CSS Code Icon" />
                 Copy CSS
               </Button>
             </div>
             <div className="button">
-              <Button width="100px" height="40px" backgroundColour="#fff" onClick={() => console.log("Button Clicked!")}>
-                <img src={require("./icons/image.svg")} alt="View Image Icon" />
-                View Image
+              <Button width="100px" height="40px" backgroundColour="#fff" onClick={this.generateGradient}>
+                <img src={require("./icons/refresh-cw.svg")} alt="View Image Icon" />
+                Generate
               </Button>
             </div>
           </div>
         </Card>
 
-        <canvas id="canvas" ref={node => this.canvas = node}></canvas>
+        {this.renderColourPalette()}
       </div>
     );
   }
